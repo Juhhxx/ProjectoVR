2ce2304cf22c305a3507dbc33c184100bc202873
Julia Costa <juliacosta232005@gmail.com>
2026-06-29 13:17:56 +0100

    Restaurant Model

--- Assets/Scripts/Trashcan.cs
+++ Assets/Scripts/Trashcan.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Trashcan : MonoBehaviour
 {
     [SerializeField] private TrashType _trashcanType;
     
+    public UnityEvent OnCorrectTrash;
+    public UnityEvent OnWrongTrash;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Trash trash = other.GetComponent<Trash>();
+
+        if (trash != null)
+        {
+            if (trash.Type == _trashcanType) OnCorrectTrash?.Invoke();
+            else OnWrongTrash?.Invoke();
+            
+            Destroy(other.gameObject);
+        }
+    }
 }
